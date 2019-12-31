415c46f4c7221d9656f7bf0cb2394ccfabede5cc
SG <sqguo@edu.uwaterloo.ca>
2019-12-31 17:49:01 -0500

    Fix config.py file

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     APP_SECRET = os.urandom(24)
-    BASE_URL = os.environ.get['BASE_URL']
-    OAUTH_API_BASE = os.environ.get['OAUTH_API_BASE']
-    OAUTH_CLIENT_ID = os.environ.get['OAUTH_CLIENT_ID']
-    OAUTH_CLIENT_SECRET = os.environ.get['OAUTH_CLIENT_SECRET']
+    BASE_URL = os.environ['BASE_URL']
+    OAUTH_API_BASE = os.environ['OAUTH_API_BASE']
+    OAUTH_CLIENT_ID = os.environ['OAUTH_CLIENT_ID']
+    OAUTH_CLIENT_SECRET = os.environ['OAUTH_CLIENT_SECRET']
